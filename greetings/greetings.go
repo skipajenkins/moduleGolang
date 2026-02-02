4ea1ab21e1c4d01d2ffaa18ec0e56edb9f8b67eb
alucard <LeroyKokoti3@gmail.com>
2026-02-02 04:20:37 +0000

    Random Greetings && Multiple People Greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,13 +1,60 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
+	"math/rand"
 )
 
 //Hello returns a greeting for the named person.
 
-func Hello(name string) string {
-	//Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	//if no name was given, return an error with a messaege.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	//(Basically we created an error handler above which tells us
+	// how to respond when the name parameter is empty.)
+
+	// if a name was received, return a value that embeds the name
+	// in a greeting message.
+
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil // Return the message with the name or return nil
+
+}
+
+// Hellos returns a map that associates each of the named people
+// with a gretting message.
+func Hellos(name []string) (map[string]string, error) {
+	//A map to associate anmes with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range name {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map. associate the retrueved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+//randomFormat reutnrns one of a set of greeting messages, The returned
+//message is selected at random.
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	//Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	//Set properties of the predefines logger, including
+	//the log entry prefix and a flag to disable printing
+	// the time , source file and the line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+
+	names := []string{
+		"Gladys",
+		"Samantha",
+		"Darrin"}
+	//Request a greeting message.
+	messages, err := greetings.Hellos(names)
+	//if an error was returned, print it to the console and
+	//exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//else if no error , print the return message to the console.
+	fmt.Println(messages)
 }
